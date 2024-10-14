c9859e55a651faad7b1ffff5de434377e0eceb03
SHIVA-ADIGA-18 <shiva959154@gmail.com>
2024-10-14 17:47:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function navanimation() {
+    var nav = document.querySelector("nav")
+
+nav.addEventListener("mouseenter",function(){
+    let tl=gsap.timeline()
+    tl.to("#nav-bottom",{
+        height: "21vh"
+    })
+    tl.to(".nav-part2 h5",{
+        display:"block"
+    })
+    tl.from(".nav-part2 h5 span",{
+        y:10,
+        // duration:0.3,
+        stagger:{
+            amount:0.6
+        }
+    })
+})
+
+nav.addEventListener("mouseleave",function(){
+    let tl=gsap.timeline()
+    // tl.to("#nav-bottom",{
+    //     height: "21vh"
+    // })
+    // tl.to(".nav-part2 h5",{
+    //     display:"block"
+    // })
+    tl.to(".nav-part2 h5 span",{
+        y:0,
+        // duration:0.3,
+        stagger:{
+            amount:0.1
+        }
+    })
+    tl.to(".nav-part2 h5",{
+        display:"none"
+    })
+    tl.to("#nav-bottom",{
+        height:0,
+        duration:0.5
+    })
+})
+    
+}
+
+navanimation()
